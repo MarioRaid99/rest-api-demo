e40155f361f857ac1ad961ddcebc6afe0595984e
Opilane TTHK <opilane@tthk.ee>
2025-11-10 17:09:05 +0200

    Update index.js

--- index.js
+++ index.js
@@ -27,13 +27,14 @@ app.post('/thingamabobs', (req,res) => {
         return res.status(400).send({error:"One or multiple parameters missing!"})
     }
     let newThingy = {
-        id: thingamabobs.lenght+1,
+        id: thingamabobs.lenght + 1,
         price: req.body.price,
         name: req.body.name
     }
     thingamabobs.push(newThingy)
-    res.status(201).location('localhost:8080/thingamabobs'+(thingamabobs.lenght-1)).
-    send(newThingy)
+    res.status(201).location('localhost:8080/thingamabobs' + (thingamabobs.lenght-1)).send(
+        newThingy
+    )
 })
 
 app.delete('/thingamabobs/:id', (req,res) => {
@@ -45,4 +46,74 @@ app.delete('/thingamabobs/:id', (req,res) => {
     res.status(204).send({error:"No content"})
 })
 
+app.put('/thingamabobs/:id', (req, res) => {
+    if (typeof thingamabobs[req.params.id - 1] === 'undefined') 
+    {
+        return res.status(404).send({ error: "Client not found" })
+    }
+    thingamabobs[req.params.id - 1].name = req.body.name;
+    thingamabobs[req.params.id - 1].email = req.body.email;
+
+    res.send(thingamabobs[req.params.id - 1]);
+})
+
 app.listen(8080, () => {console.log('API running at: http://localhost:8080')})
+
+
+
+///CLIENTS OSA ///
+
+const clients = [
+    {id: 1, name: "Mihkel Pauk", email: "Mihkel.Pauk@gmail.com"},
+    {id: 2, name: "Valdis Vaha", email: "Valdis.Vaha@gmail.com"},
+    {id: 3, name: "Marko Muhk", email: "Marko.Muhk"}
+]
+
+app.get('/clients', (req, res) => {res.send(clients)})
+
+app.get('/clients/:id', (req, res) => {
+    if (typeof clients[req.params.id -1] === 'undefined')
+    {
+        return res.status(404).send({error: "Client not found! Check your client id!"})
+    }
+}
+)
+
+app.post('/clients', (req,res) => {
+    if (!req.body.name || !req.body.email)
+    {
+        return res.status(400).send({error:"One or multiple parameters missing!"})
+    }
+    let newThingy = {
+        id: clients.lenght+1,
+        email: req.body.email,
+        name: req.body.name
+    }
+    clients.push(newThingy)
+    res.status(201).location('localhost:8080/clients'+(clients.lenght-1)).
+    send(newThingy)
+})
+
+app.delete('/clients/:id', (req,res) => {
+    if (typeof clients[req.params.id -1] === 'undefined')
+    {
+        return res.status(404).send({error: "Client not found! Check your client id!"})
+    }
+    clients.splice(req.params.id -1.1)
+    res.status(204).send({error:"No content"})
+})
+
+app.put('/clients/:id', (req, res) => {
+    if (typeof clients[req.params.id - 1] === 'undefined') 
+    {
+        return res.status(404).send({ error: "Client not found" })
+    }
+    clients[req.params.id - 1].name = req.body.name;
+    clients[req.params.id - 1].email = req.body.email;
+
+    res.send(clients[req.params.id - 1])
+})
+
+
+
+
